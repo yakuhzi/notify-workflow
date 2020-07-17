0493f26a52d52145574d343606e3a92eb4c9c9e2
yakuhzi <miguel@heidegger-ueberlingen.de>
2020-07-17 19:11:31 +0200

    Send branch when no tag is available

--- src/main/main.ts
+++ src/main/main.ts
@@ -18,18 +18,27 @@ async function run(): Promise<void> {
     }
 
     const repository = process.env.GITHUB_REPOSITORY
-    const tag = process.env.GITHUB_REF?.replace('refs/tags/', '')
     const workflow = process.env.GITHUB_WORKFLOW
     const runId = process.env.GITHUB_RUN_ID
     const runNumber = process.env.GITHUB_RUN_NUMBER
     const commit = process.env.GITHUB_SHA
     const checkURL = `https://github.com/${repository}/commit/${commit}/checks`
 
+    let tag = process.env.GITHUB_REF
+
+    if (tag?.startsWith('refs/tags/')) {
+      tag = `\nTag: ${tag.replace('refs/tags/', '')}`
+    } else if (tag?.startsWith('refs/heads/')){
+      tag = `\nBranch: ${tag.replace('refs/heads/', '')}`
+    } else {
+      tag = ''
+    }
+
     await axios.get(`https://api.telegram.org/bot${process.env.INPUT_BOT_TOKEN}/sendMessage`, {
       params: {
         chat_id: process.env.INPUT_CHAT_ID,
         text: `*GitHub Actions Workflow*\nStatus: ${status}\nRepository: https://github.com/${repository}` +
-          `${tag ? `\nTag: ${tag}` : ''}\nWorkflow: ${workflow} - ${runId} (${runNumber})\nChecks: ${checkURL}`,
+          `${tag}\nWorkflow: ${workflow} - ${runId} (${runNumber})\nChecks: ${checkURL}`,
         parse_mode: 'Markdown',
         disable_web_page_preview: true,
       },
