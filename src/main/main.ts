19b1639ba7fb30521ebe6a06b062e280c7465d1e
yakuhzi <miguel@heidegger-ueberlingen.de>
2020-07-17 12:05:51 +0200

    Add more details to status message

--- src/main/main.ts
+++ src/main/main.ts
@@ -3,9 +3,6 @@ import core from '@actions/core'
 
 async function run(): Promise<void> {
   try {
-    const repository = process.env.GITHUB_REPOSITORY
-    const tag = process.env.GITHUB_REF?.replace('refs/tags/', '')
-
     let status = 'Undefined ❎'
 
     switch (process.env.INPUT_JOB_STATUS) {
@@ -17,11 +14,21 @@ async function run(): Promise<void> {
         break
     }
 
+    const repository = process.env.GITHUB_REPOSITORY
+    const tag = process.env.GITHUB_REF?.replace('refs/tags/', '')
+    const workflow = process.env.GITHUB_WORKFLOW
+    const runId = process.env.GITHUB_RUN_ID
+    const runNumber = process.env.GITHUB_RUN_NUMBER
+    const commit = process.env.GITHUB_SHA
+    const checkURL = `https://github.com/${repository}/commit/${commit}/checks`
+
     await axios.get(`https://api.telegram.org/bot${process.env.INPUT_BOT_TOKEN}/sendMessage`, {
       params: {
         chat_id: process.env.INPUT_CHAT_ID,
-        text: `*GitHub Actions Workflow Status*\nStatus: ${status}\nRepository: ${repository}\nRelease: ${tag}`,
+        text: `*GitHub Actions Workflow*\nStatus: ${status}\nRepository: https://github.com/${repository}` +
+          `\n${tag ? `Tag: ${tag}` : ''}\nWorkflow: ${workflow} - ${runId} (${runNumber})\nChecks: ${checkURL}`,
         parse_mode: 'Markdown',
+        disable_web_page_preview: true,
       },
     })
 
