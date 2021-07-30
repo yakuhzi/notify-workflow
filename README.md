cbb332f9e0aa117ff024d495d620f62125d5593b
yakuhzi <miguel@heidegger-ueberlingen.de>
2021-07-30 22:28:11 +0200

    Add option to send firebase message for release

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Notify Telegram
+# Notify Status
 
-A GitHub Action to send workflow status over a Telegram Bot.
+A GitHub Action to send workflow status over a Telegram bot and Firebase.
 
 Usage
 -------
@@ -8,10 +8,24 @@ Here is an example how to use this action:
 
 ```yaml  
 - name: Notify workflow status
-  uses: yakuhzi/notify-telegram@v1
+  uses: yakuhzi/notify-status@v2
   if: always()
   with:
     chat_id: ${{ secrets.CHAT_ID }}
     bot_token: ${{ secrets.BOT_TOKEN }}
+    job_status: ${{ job.status }}
+```
+
+If you also want to send a Firebase message:
+```yaml  
+- name: Notify workflow status
+  uses: yakuhzi/notify-status@v2
+  if: always()
+  with:
+    chat_id: ${{ secrets.CHAT_ID }}
+    bot_token: ${{ secrets.BOT_TOKEN }}
+    firebase_server_key: ${{ secrets.FIREBASE_SERVER_KEY }}
+    firebase_topic: ${{ secrets.FIREBASE_TOPIC }}
+    app_name: ${{ secrets.APP_NAME }}
     job_status: ${{ job.status }}
 ```
--- src/main/main.ts
+++ src/main/main.ts
@@ -24,26 +24,46 @@ async function run(): Promise<void> {
     const commit = process.env.GITHUB_SHA
     const checkURL = `https://github.com/${repository}/commit/${commit}/checks`
 
-    let tag = process.env.GITHUB_REF
+    let ref = process.env.GITHUB_REF
+    let tag: string | undefined
 
-    if (tag?.startsWith('refs/tags/')) {
-      tag = `\nTag: ${tag.replace('refs/tags/', '')}`
-    } else if (tag?.startsWith('refs/heads/')){
-      tag = `\nBranch: ${tag.replace('refs/heads/', '')}`
+    if (ref?.startsWith('refs/tags/')) {
+      tag = ref.replace('refs/tags/', '')
+      ref = `\nTag: ${tag}`
+    } else if (ref?.startsWith('refs/heads/')) {
+      ref = `\nBranch: ${ref.replace('refs/heads/', '')}`
     } else {
-      tag = ''
+      ref = ''
     }
 
     await axios.get(`https://api.telegram.org/bot${process.env.INPUT_BOT_TOKEN}/sendMessage`, {
       params: {
         chat_id: process.env.INPUT_CHAT_ID,
         text: `*GitHub Actions Workflow*\nStatus: ${status}\nRepository: https://github.com/${repository}` +
-          `${tag}\nWorkflow: ${workflow} - ${runId} (${runNumber})\nChecks: ${checkURL}`,
+          `${ref}\nWorkflow: ${workflow} - ${runId} (${runNumber})\nChecks: ${checkURL}`,
         parse_mode: 'Markdown',
         disable_web_page_preview: true,
       },
     })
 
+    const appName = process.env.INPUT_APP_NAME
+
+    if (!appName || !tag) {
+      return
+    }
+
+    await axios.post('https://fcm.googleapis.com/fcm/send', {
+      to: `/topics/${process.env.INPUT_FIREBASE_TOPIC}`,
+      data: {
+        name: appName,
+        version: tag.replace('v', ''),
+      },
+    }, {
+      headers: {
+        'Authorization': `key=${process.env.INPUT_FIREBASE_SERVER_KEY}`,
+        'Content-Type': 'application/json',
+      }
+    })
   } catch (error) {
     core.setFailed(error.message)
   }
