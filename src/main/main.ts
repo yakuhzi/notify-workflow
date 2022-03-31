ff0caad4d47bee194fcf11d1acdd0ca9b1e40004
yakuhzi <miguel@heidegger-ueberlingen.de>
2022-03-31 14:39:06 +0200

    Fix: Pipeline fails due to unwanted firebase call

--- src/main/main.ts
+++ src/main/main.ts
@@ -3,28 +3,36 @@ import axios from 'axios'
 
 async function run(): Promise<void> {
   try {
-    let status = 'Undefined ❎'
-
-    switch (process.env.INPUT_JOB_STATUS) {
-      case 'success':
-        status = 'Success ✅'
-        break
-      case 'failure':
-        status = 'Failure 🚫'
-        break
-      case 'cancelled':
-        status = 'Cancelled ❌'
-        break
-    }
-
     const repository = process.env.GITHUB_REPOSITORY
     const workflow = process.env.GITHUB_WORKFLOW
     const runId = process.env.GITHUB_RUN_ID
     const runNumber = process.env.GITHUB_RUN_NUMBER
     const commit = process.env.GITHUB_SHA
-    const checkURL = `https://github.com/${repository}/commit/${commit}/checks`
-
     let ref = process.env.GITHUB_REF
+
+    const jobStatus = process.env.INPUT_JOB_STATUS
+
+    const botToken = process.env.INPUT_BOT_TOKEN
+    const chatId = process.env.INPUT_CHAT_ID
+
+    const appName = process.env.INPUT_APP_NAME
+    const firebaseServerKey = process.env.INPUT_FIREBASE_SERVER_KEY
+    const firebaseTopic = process.env.INPUT_FIREBASE_TOPIC
+
+    let statusMessage = 'Undefined ❎'
+
+    switch (jobStatus) {
+      case 'success':
+        statusMessage = 'Success ✅'
+        break
+      case 'failure':
+        statusMessage = 'Failure 🚫'
+        break
+      case 'cancelled':
+        statusMessage = 'Cancelled ❌'
+        break
+    }
+
     let tag: string | undefined
 
     if (ref?.startsWith('refs/tags/')) {
@@ -36,36 +44,36 @@ async function run(): Promise<void> {
       ref = ''
     }
 
-    await axios.get(`https://api.telegram.org/bot${process.env.INPUT_BOT_TOKEN}/sendMessage`, {
+    const checkURL = `https://github.com/${repository}/commit/${commit}/checks`
+
+    await axios.get(`https://api.telegram.org/bot${botToken}/sendMessage`, {
       params: {
-        chat_id: process.env.INPUT_CHAT_ID,
-        text: `*GitHub Actions Workflow*\nStatus: ${status}\nRepository: https://github.com/${repository}` +
+        chat_id: chatId,
+        text: `*GitHub Actions Workflow*\nStatus: ${statusMessage}\nRepository: https://github.com/${repository}` +
           `${ref}\nWorkflow: ${workflow} - ${runId} (${runNumber})\nChecks: ${checkURL}`,
         parse_mode: 'Markdown',
         disable_web_page_preview: true,
       },
     })
 
-    const appName = process.env.INPUT_APP_NAME
-
-    if (process.env.INPUT_JOB_STATUS != 'success' && !appName || !tag) {
+    if (jobStatus !== 'success' || !firebaseServerKey || !firebaseTopic || !appName || !tag) {
       return
     }
 
     await axios.post('https://fcm.googleapis.com/fcm/send', {
-      to: `/topics/${process.env.INPUT_FIREBASE_TOPIC}`,
+      to: `/topics/${firebaseTopic}`,
       data: {
         name: appName,
         version: tag.replace('v', ''),
       },
     }, {
       headers: {
-        'Authorization': `key=${process.env.INPUT_FIREBASE_SERVER_KEY}`,
+        'Authorization': `key=${firebaseServerKey}`,
         'Content-Type': 'application/json',
       }
     })
-  } catch (error) {
-    core.setFailed(error.message)
+  } catch (error: any) {
+    core.setFailed(error)
   }
 }
 
